3249a12995e0dd39c3e3c6c64850219a27ec48f9
Tolga Şen <tolgasen9558@gmail.com>
2018-06-20 23:03:42 -0400

    Implemented Cube Neighbor Search Algorithm

--- Match 3 Clone Project/Assets/Scripts/BoardController.cs
+++ Match 3 Clone Project/Assets/Scripts/BoardController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class BoardController : MonoBehaviour
 {
@@ -22,7 +23,6 @@ public class BoardController : MonoBehaviour
 
 
     Cube[,] grid;
-
 
     void Awake()
     {
@@ -75,7 +75,6 @@ public class BoardController : MonoBehaviour
             for(int col = 0; col < width; col++)
             {
                 cubePos = rowPos + Vector3.left * ((width - 1) / 2f - col);
-                Debug.Log(cubePos);
                 GameObject cubeObj = Instantiate(cubePrefab, cubePos, Quaternion.identity);
                 cubeObj.transform.SetParent(newRow.transform);
 
@@ -113,13 +112,86 @@ public class BoardController : MonoBehaviour
 
     void PopCube(Cube cube)
     {
-        foreach(Cube _cube in grid)
+        List<Cube> neighbours = GetNeighbourCubes(cube);
+        foreach(Cube _cube in neighbours)
         {
-            if(!_cube.Equals(cube) && _cube.MyColor.Equals(cube.MyColor))
-            {
-                Destroy(_cube.gameObject);
-            }
+            Destroy(_cube.gameObject);
         }
         Destroy(cube.gameObject);
+    }
+
+    List<Cube> GetNeighbourCubes(Cube cube)
+    {
+        List<Cube> neighbours = new List<Cube>();
+        bool[,] visited = new bool[grid.GetLength(0), grid.GetLength(1)];
+
+        GetNeighbourCubesHelper(cube, ref neighbours, ref visited);
+        return neighbours;
+    }
+
+    void GetNeighbourCubesHelper(Cube cube, ref List<Cube> memoNeighbours, ref bool[,] memoVisited)
+    {
+
+        Vector2 coordinates = GetCubeCoordinates(cube);
+        int yCoord = (int)coordinates.x;
+        int xCoord = (int)coordinates.y;
+
+        memoVisited[yCoord, xCoord] = true;
+
+        // Check right
+        if(xCoord + 1 < grid.GetLength(1))
+        {
+            if(!memoVisited[yCoord, xCoord + 1] && grid[yCoord, xCoord + 1].MyColor == cube.MyColor)
+            {
+                memoNeighbours.Add(grid[yCoord, xCoord + 1]);
+                GetNeighbourCubesHelper(grid[yCoord, xCoord + 1], ref memoNeighbours, ref memoVisited);
+            }
+        }
+           
+
+        // Check left
+        if(xCoord - 1 >= 0)
+        {
+            if(!memoVisited[yCoord, xCoord - 1] && grid[yCoord, xCoord - 1].MyColor == cube.MyColor)
+            {
+                memoNeighbours.Add(grid[yCoord, xCoord - 1]);
+                GetNeighbourCubesHelper(grid[yCoord, xCoord - 1], ref memoNeighbours, ref memoVisited);
+            }
+        }
+
+        // Check top
+        if(yCoord - 1 >= 0)
+        {
+            if(!memoVisited[yCoord - 1, xCoord] && grid[yCoord - 1, xCoord].MyColor == cube.MyColor)
+            {
+                memoNeighbours.Add(grid[yCoord - 1, xCoord]);
+                GetNeighbourCubesHelper(grid[yCoord - 1, xCoord], ref memoNeighbours, ref memoVisited);
+            }
+        }
+
+        // Check bottom
+        if(yCoord + 1 < grid.GetLength(0))
+        {
+            if(!memoVisited[yCoord + 1, xCoord] && grid[yCoord + 1, xCoord].MyColor == cube.MyColor)
+            {
+                memoNeighbours.Add(grid[yCoord + 1, xCoord]);
+                GetNeighbourCubesHelper(grid[yCoord + 1, xCoord], ref memoNeighbours, ref memoVisited);
+            }
+        }
+    }
+
+    Vector2 GetCubeCoordinates(Cube cube)
+    {
+        for(int i = 0; i < grid.GetLength(0); i++)
+        {
+            for(int j = 0; j < grid.GetLength(1); j++)
+            {
+                if(grid[i,j].Equals(cube))
+                {
+                    return new Vector2(i, j);
+                }
+            }
+        }
+        return Vector2.negativeInfinity;
     }
 }
